3204f4498d065115b4711f7a3b28399722e231dd
Saifullah <saifullah.saifullah@dfki.uni-kl.de>
2025-06-03 00:49:25 +0200

    Addin new dates but wokring on it

--- src/atria_core/schemas/dataset.py
+++ src/atria_core/schemas/dataset.py
@@ -1,12 +1,15 @@
 from enum import Enum
 from typing import List
 
+from fastapi import HTTPException
+from httpx import AsyncClient
+
 from atria_core.schemas.base import BaseDatabaseSchema, DataInstanceType, OptionalModel
 from atria_core.schemas.config import Config
 from atria_core.schemas.utils import NameStr, SerializableUUID
 from atria_core.types.datasets.metadata import DatasetLabels, DatasetMetadata  # noqa
 from atria_core.types.datasets.splits import DatasetSplitType
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, computed_field
 
 
 class ShardFileType(str, Enum):
@@ -28,6 +31,20 @@ class ProcessingStatus(str, Enum):
     COMPLETED = "completed"
 
 
+class DatasetStatus(str, Enum):
+    READY = "ready"
+    FAILED = "failed"
+    PROCESSING = "processing"
+
+
+class DatasetDownloadStatus(str, Enum):
+    EMPTY_DATASET = "empty_dataset"
+    NOT_PREPARED = "not_prepared"
+    PREPARING = "preparing"
+    READY = "ready"
+    DATA_MODIFIED = "data_modified"
+
+
 # ShardFile
 class ShardFileBase(BaseModel):
     index: int
@@ -46,6 +63,16 @@ class ShardFileUpdate(ShardFileBase, OptionalModel):
 
 class ShardFile(ShardFileBase, BaseDatabaseSchema):
     split_id: SerializableUUID
+
+    async def download(self) -> bytes:
+        async with AsyncClient() as client:
+            response = await client.get(self.url)
+        if response.status_code != 200:
+            raise HTTPException(
+                status_code=response.status_code,
+                detail=f"Failed to load model from {self.url}: {response.text}",
+            )
+        return response.content
 
 
 # DatasetSplit
@@ -68,6 +95,23 @@ class DatasetSplitUpdate(OptionalModel):
 class DatasetSplit(DatasetSplitBase, BaseDatabaseSchema):
     dataset_id: SerializableUUID
     shard_files: List[ShardFile] = Field(default_factory=list)
+
+    @computed_field
+    def size(self) -> int:
+        return sum(split.filesize for split in self.shard_files)
+
+    async def download(self) -> bytes:
+        shard_files = []
+        for shard_file in self.shard_files:
+            try:
+                content = await shard_file.download()
+                return shard_files.append(content)
+            except HTTPException as e:
+                raise HTTPException(
+                    status_code=e.status_code,
+                    detail=f"Failed to download shard file {shard_file.index}: {e.detail}",
+                )
+        return shard_files
 
 
 # Dataset
@@ -98,6 +142,63 @@ class Dataset(DatasetBase, BaseDatabaseSchema):
     user_id: SerializableUUID
     config: Config
     splits: List[DatasetSplit] = Field(default_factory=list)
+
+    @computed_field
+    def size(self) -> int:
+        return sum(split.size for split in self.splits)
+
+    @computed_field
+    def status(self) -> DatasetStatus:
+        for split in self.splits:
+            if (
+                split.upload_status == UploadStatus.PENDING
+                or split.processing_status
+                in [
+                    ProcessingStatus.REQUESTED,
+                    ProcessingStatus.PENDING,
+                ]
+            ):
+                return DatasetStatus.PROCESSING
+            if (
+                split.processing_status == ProcessingStatus.FAILED
+                or split.upload_status == UploadStatus.FAILED
+            ):
+                return DatasetStatus.FAILED
+            return DatasetStatus.READY
+
+    @computed_field
+    def download_status(self) -> DatasetDownloadStatus:
+        if len(self.splits) == 0:
+            return DatasetDownloadStatus.NOT_AVAILABLE
+        for split in self.splits:
+            if (
+                split.upload_status == UploadStatus.PENDING
+                or split.processing_status
+                in [
+                    ProcessingStatus.REQUESTED,
+                    ProcessingStatus.PENDING,
+                ]
+            ):
+                return DatasetStatus.PROCESSING
+            if (
+                split.processing_status == ProcessingStatus.FAILED
+                or split.upload_status == UploadStatus.FAILED
+            ):
+                return DatasetStatus.FAILED
+            return DatasetStatus.READY
+
+    async def download(self) -> bytes:
+        splits = []
+        for split in self.splits:
+            try:
+                content = await split.download()
+                splits.append(content)
+            except HTTPException as e:
+                raise HTTPException(
+                    status_code=e.status_code,
+                    detail=f"Failed to download split {split.name}: {e.detail}",
+                )
+        return splits
 
 
 class SplitUploadRequest(BaseModel):
