805e26f83beed9bafb2b1962d549eca831caf252
Saifullah <saifullah.saifullah@dfki.uni-kl.de>
2025-07-22 00:29:58 +0200

    Removing unndeeded stuff.

--- src/atria_core/utilities/auto_config.py
+++ src/atria_core/utilities/auto_config.py
@@ -5,29 +5,36 @@ from hydra_zen import builds
 from omegaconf import OmegaConf
 
 
-def auto_config(attr_name="config", exclude: set[str] | None = None):
-    def decorator(cls):
+class AutoConfig:
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+
         original_init = cls.__init__
 
         @functools.wraps(original_init)
         def wrapped_init(self, *args, **kwargs):
-            if type(self) is cls:
-                sig = inspect.signature(original_init)
-                bound = sig.bind(self, *args, **kwargs)
-                bound.apply_defaults()
-                config = {k: v for k, v in bound.arguments.items() if k != "self"}
-                if "kwargs" in config:
-                    config.update(config.pop("kwargs", {}))
-                config = OmegaConf.create(
-                    builds(cls, populate_full_signature=True, **config)
-                )
-                if exclude is not None:
-                    config = OmegaConf.to_container(config)
-                    config = {k: v for k, v in config.items() if k not in exclude}
-                setattr(self, attr_name, config)
             original_init(self, *args, **kwargs)
 
-        cls.__init__ = wrapped_init
-        return cls
+            # Generate config after initialization
+            sig = inspect.signature(original_init)
+            bound = sig.bind(self, *args, **kwargs)
+            bound.apply_defaults()
 
-    return decorator
+            config = {k: v for k, v in bound.arguments.items() if k != "self"}
+            if "kwargs" in config:
+                config.update(config.pop("kwargs", {}))
+
+            config = OmegaConf.create(
+                builds(cls, populate_full_signature=True, **config)
+            )
+            config = OmegaConf.to_container(config)
+            self._config = config
+
+        cls.__init__ = wrapped_init
+
+    @property
+    def config(self):
+        """
+        Returns the configuration of the dataset as a dictionary.
+        """
+        return self._config
